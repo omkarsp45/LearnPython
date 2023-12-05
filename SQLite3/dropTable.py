fe22dfff81ef81a8256fd517706b0328c2c2c826
Omkar Patil <omkar.spatil@outlook.com>
2023-12-05 08:51:56 +0530

    Delete Table From Database

--- SQLite3/dropTable.py
+++ SQLite3/dropTable.py
@@ -3,6 +3,7 @@ import sqlite3
 connection = sqlite3.connect('customer.db')
 cursor = connection.cursor()
 
+# Deletes Table From Database
 cursor.execute("DROP TABLE Customers")
 
 connection.commit()
